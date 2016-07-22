24d57302898f9d8f0d589110e4a3a81fd0a1517a
Sosa <sosa.sangsuosa@gmail.com>
2016-07-22 00:10:15 -0700

    HttpURLConnection

--- app/src/main/java/com/caoye/tulingdemo/HttpURLConnListener.java
+++ app/src/main/java/com/caoye/tulingdemo/HttpURLConnListener.java
@@ -3,6 +3,6 @@ package com.caoye.tulingdemo;
 /**
  * Created by admin on 7/18/16.
  */
-public interface HttpGetDataListener {
+public interface HttpURLConnListener {
     void getDataFromURL(String data);
 }
